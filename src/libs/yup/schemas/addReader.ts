67576148cfc2f5365b4f3a5977acac53874b0cd5
Aleksander <atkrzanowski@gmail.com>
2024-01-04 12:20:06 +0100

    bugfix

--- src/libs/yup/schemas/addReader.ts
+++ src/libs/yup/schemas/addReader.ts
@@ -4,9 +4,9 @@ const addReaderSchema = yup.object().shape({
   readerImage: yup.mixed<File>().image().maxFileSize(),
   firstName: yup.string().required(),
   lastName: yup.string().required(),
-  phoneNumber: yup.string().required(),
+  phoneNumber: yup.string().length(9, "forms:errors.phoneNumber").required(),
   address: yup.object({
-    street: yup.string().length(9, "forms:errors.phoneNumber").required(),
+    street: yup.string().required(),
     city: yup.string().required(),
     postalCode: yup.string().length(6, "forms:errors.postalCode").required(),
   }),

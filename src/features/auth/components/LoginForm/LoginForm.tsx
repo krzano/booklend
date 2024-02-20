6b8928b1a6a82799114120e2310f28ad8222ea24
Aleksander <atkrzanowski@gmail.com>
2024-02-20 22:11:07 +0100

    added continue with demo account button to login form

--- src/features/auth/components/LoginForm/LoginForm.tsx
+++ src/features/auth/components/LoginForm/LoginForm.tsx
@@ -6,10 +6,19 @@ import { Form, Formik } from "formik"
 import { useTranslation } from "react-i18next"
 import { loginUser } from "../../authThunk"
 import loginSchema, { LoginFormValues } from "@/libs/yup/schemas/login"
+import Divider from "@mui/material/Divider/Divider"
+import Typography from "@mui/material/Typography/Typography"
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
+import { useState } from "react"
+import { Stack } from "@mui/material"
+
+const DEMO_ACCOUNT_EMAIL = import.meta.env.VITE_DEMO_ACCOUNT_EMAIL
+const DEMO_ACCOUNT_PASSWORD = import.meta.env.VITE_DEMO_ACCOUNT_PASSWORD
 
 const LoginForm = () => {
   const dispatch = useAppDispatch()
   const { t } = useTranslation(["forms"])
+  const [isDemoAccountLoading, setIsDemoAccountLoading] = useState(false)
   const initialValues: LoginFormValues = {
     email: "",
     password: "",
@@ -44,15 +53,48 @@ const LoginForm = () => {
               />
             </Grid>
             <Grid item xs={2}>
-              <Button
-                color="secondary"
-                type="submit"
-                isSubmitting={isSubmitting}
-                fullWidth
-                size="large"
-              >
-                {t("common:login")}
-              </Button>
+              <Stack spacing={2}>
+                <Button
+                  color="secondary"
+                  type="submit"
+                  isSubmitting={isSubmitting}
+                  disabled={isDemoAccountLoading}
+                  fullWidth
+                  size="large"
+                >
+                  {t("common:login")}
+                </Button>
+                <Divider>
+                  <Typography
+                    color={(theme) => theme.palette.grey[500]}
+                    fontSize={14}
+                    fontWeight={500}
+                  >
+                    {t("common:orContinueWith")}
+                  </Typography>
+                </Divider>
+                <Button
+                  type="button"
+                  onAsyncClick={async () => {
+                    setIsDemoAccountLoading(true)
+                    await dispatch(
+                      loginUser({
+                        email: DEMO_ACCOUNT_EMAIL,
+                        password: DEMO_ACCOUNT_PASSWORD,
+                      }),
+                    )
+                    setIsDemoAccountLoading(false)
+                  }}
+                  disabled={isSubmitting}
+                  color="secondary"
+                  variant="outlined"
+                  size="large"
+                  fullWidth
+                  startIcon={<AccountCircleIcon />}
+                >
+                  {t("common:demoAccount")}
+                </Button>
+              </Stack>
             </Grid>
           </Grid>
         </Form>
